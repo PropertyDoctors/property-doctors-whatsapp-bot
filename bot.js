65a91f1db5c512cd2e3c6b3171a014c76aa4031a
Dr Sam <propertedoctors@gmail.com>
2026-03-14 23:59:28 +0530

    add render keepalive server

--- bot.js
+++ bot.js
@@ -79,3 +79,16 @@ console.log("API error:", error.message)
 }
 
 startBot()
+
+/* KEEP RENDER SERVICE ALIVE */
+
+const express = require("express")
+const app = express()
+
+app.get("/", (req,res)=>{
+res.send("WhatsApp bot running")
+})
+
+app.listen(process.env.PORT || 3000, ()=>{
+console.log("Server running")
+})
