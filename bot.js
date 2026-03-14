9c45024df000d4e6a1565ebabd5b24d19d6b7b13
Dr Sam <propertedoctors@gmail.com>
2026-03-14 21:03:01 +0530

    remove chrome executable path

--- bot.js
+++ bot.js
@@ -5,8 +5,7 @@ const axios = require("axios");
 const client = new Client({
   authStrategy: new LocalAuth(),
   puppeteer: {
-    args: ["--no-sandbox", "--disable-setuid-sandbox"],
-    executablePath: "/usr/bin/google-chrome-stable"
+    args: ["--no-sandbox", "--disable-setuid-sandbox"]
   }
 });
 
